959071c5171ea028b815cf6b3ee1c9713c71c95b
tuotesz27 <tuoesz27@digiwin.com>
2023-11-11 01:41:34 +0800

    [feat] 修改 網站整體樣式

--- src/app/services/base-api.service.ts
+++ src/app/services/base-api.service.ts
@@ -12,21 +12,22 @@ export class BaseApiService {
   constructor(private httpClient: HttpClient){
 
   }
+  /** 查詢非敏感資料 */
   get<T>(url: string, header?: any): Observable<T>{
     const apiHeader = {...this.baseHeader,...header};
     return this.httpClient.get<T>(url, {headers: apiHeader});
   }
-
+  /** 新增或查詢 資料 */
   post<T>(url: string, body: any, header?: any): Observable<T>{
     const apiHeader = {...this.baseHeader,...header};
     return this.httpClient.post<T>(url, body,{headers: apiHeader});
   }
-
+  /** 更新資料 */
   put<T>(url: string, body: any, header?: any): Observable<T>{
     const apiHeader = {...this.baseHeader,...header};
     return this.httpClient.put<T>(url, body,{headers: apiHeader});
   }
-
+  /** 刪除資料 */
   delete<T>(url: string, body: any, header?: any): Observable<T>{
     const apiHeader = {...this.baseHeader,...header};
     return this.httpClient.delete<T>(url, {headers: apiHeader});
